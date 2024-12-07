5fc15bb156b81524c12435d1458e0b22e89c210e
cz0r3k <czorek@protonmail.com>
2024-12-07 19:15:50 +0100

    day 01 add binary heap

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,22 +1,36 @@
+#![feature(test)]
+use std::collections::BinaryHeap;
+
+extern crate test;
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part1(input);
     println!("{output}");
 }
 
+fn part1_heap(input: &str) -> String {
+    let (left_numbers, right_numbers) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(left, right)| (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap()))
+        .unzip::<i32, i32, BinaryHeap<i32>, BinaryHeap<i32>>();
+    left_numbers
+        .iter()
+        .zip(right_numbers.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum::<i32>()
+        .to_string()
+}
+
 fn part1(input: &str) -> String {
-    let lines = input.lines();
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-
-    for line in lines {
-        let mut line = line.split_whitespace();
-        left.push(line.next().unwrap().parse::<i32>().unwrap());
-        right.push(line.next().unwrap().parse::<i32>().unwrap());
-    }
-    left.sort();
-    right.sort();
-
+    let (mut left, mut right) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(left, right)| (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap()))
+        .unzip::<i32, i32, Vec<i32>, Vec<i32>>();
+    left.sort_unstable();
+    right.sort_unstable();
     left.iter()
         .zip(right.iter())
         .map(|(l, r)| (l - r).abs())
@@ -26,7 +40,8 @@ fn part1(input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part1_heap};
+    use test::Bencher;
 
     #[test]
     fn part1_test() {
@@ -37,5 +52,16 @@ mod tests {
 3   9
 3   3";
         assert_eq!("11", part1(input));
+    }
+    #[bench]
+    fn part1_bench(b: &mut Bencher) {
+        let input = include_str!("./input.txt");
+        b.iter(|| part1(input));
+    }
+
+    #[bench]
+    fn part1_heap_bench(b: &mut Bencher) {
+        let input = include_str!("./input.txt");
+        b.iter(|| part1_heap(input));
     }
 }
